0da2ae77df5431831b4f8fb903a43521244650d6
paulstancer <pstancer@hotmail.com>
2016-02-12 08:48:26 +0800

    Further updates to properties.

--- Xigadee.Platform/Properties/AssemblyInfo.cs
+++ Xigadee.Platform/Properties/AssemblyInfo.cs
@@ -5,12 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IMGroup Microservice Platform")]
+[assembly: AssemblyTitle("Xigadee Microservice Platform")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-
 [assembly: AssemblyProduct("Xigadee.Platform")]
-
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
