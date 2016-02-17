3a32f42a829a05993a2edde6f517bf6e3a2bc03e
paulstancer <pstancer@hotmail.com>
2016-02-17 01:15:40 +0000

    Another script update. Just need the version id now.

--- AssemblyInfo/UpdateVersion.ps1
+++ AssemblyInfo/UpdateVersion.ps1
@@ -6,4 +6,6 @@ Write-Host "AGENT_BUILDDIRECTORY contents:"
 gci $Env:AGENT_BUILDDIRECTORY
 Write-Host "BUILD_SOURCESDIRECTORY contents:"
 gci $Env:BUILD_SOURCESDIRECTORY
+$url = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)$env:SYSTEM_TEAMPROJECTID/_apis/build/definitions/$($env:SYSTEM_DEFINITIONID)?api-version=2.0"
+Write-Host "URL: $url"
 Write-Host "Over and out."
