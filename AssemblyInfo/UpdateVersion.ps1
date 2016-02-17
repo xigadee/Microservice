11878bb22649fa5997265f8ecaca5bfcca393a38
paulstancer <pstancer@hotmail.com>
2016-02-17 10:56:37 +0000

    Powershell script bug fix

--- AssemblyInfo/UpdateVersion.ps1
+++ AssemblyInfo/UpdateVersion.ps1
@@ -13,6 +13,6 @@ Write-Host "Nuget Version:" $nugetversion;
 (Get-Content AssemblyInfo\SharedAssemblyInfo.cs).replace('0.0.0.0', $version) | Set-Content AssemblyInfo\SharedAssemblyInfo.cs
 
 Get-ChildItem "**\*.nuspec" -Recurse | ForEach-Object -Process {
-    (Get-Content $_) -Replace '$nugetversion', $nugetversion | Set-Content $_
+    (Get-Content $_) -Replace '$nugetversion$', $nugetversion | Set-Content $_
 }
 Write-Host "Over and out."
