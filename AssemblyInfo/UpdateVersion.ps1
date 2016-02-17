a6d425907e12f172bf4c3623307a9252731fba16
paulstancer <pstancer@hotmail.com>
2016-02-17 11:25:03 +0000

    Powershell script update

--- AssemblyInfo/UpdateVersion.ps1
+++ AssemblyInfo/UpdateVersion.ps1
@@ -12,7 +12,9 @@ Write-Host "Nuget Version:" $nugetversion;
 
 (Get-Content AssemblyInfo\SharedAssemblyInfo.cs).replace('0.0.0.0', $version) | Set-Content AssemblyInfo\SharedAssemblyInfo.cs
 
-Get-ChildItem "**\*.nuspec" -Recurse | ForEach-Object -Process {
+Get-ChildItem -Path "..\*\*.nuspec" -Recurse | ForEach-Object -Process {Write-Host $_.name -foregroundcolor cyan}
+
+Get-ChildItem -Path "..\*\*.nuspec" -Recurse | ForEach-Object -Process {
     (Get-Content $_) -Replace '$nugetversion$', $nugetversion | Set-Content $_
 }
 Write-Host "Over and out."
