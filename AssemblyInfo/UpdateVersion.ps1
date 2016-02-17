500eda9f948657cdbf090e8f3d600b733fb74038
paulstancer <pstancer@hotmail.com>
2016-02-17 15:07:45 +0000

    Renamed main projects to better fit business case.

--- AssemblyInfo/UpdateVersion.ps1
+++ AssemblyInfo/UpdateVersion.ps1
@@ -1,20 +1,27 @@
 param(
 	[string]$version = $null, 
-	[string]$subversion = $null
+	[string]$subversion = $null,
+	[bool]$updatepackages = $false
 )
 Write-Host "BUILD_BUILDNUMBER contents: $Env:BUILD_BUILDNUMBER"
 
+#OK, if the version has not been passed in, then get it from the environment.
 IF([string]::IsNullOrWhitespace($version)){$version=$Env:BUILD_BUILDNUMBER}
 Write-Host "Assembly Version:" $version;
 
+#Do we have a sub version for prerelease nuget packages?
 IF(![string]::IsNullOrWhitespace($subversion)){$nugetversion=$version+"-"+$subversion}
 Write-Host "Nuget Version:" $nugetversion;
 
+#Set the Assembly versions for the DLLs.
 (Get-Content AssemblyInfo\SharedAssemblyInfo.cs).replace('0.0.0.0', $version) | Set-Content AssemblyInfo\SharedAssemblyInfo.cs
 
-Get-ChildItem -Path "..\*\*.nuspec" -Recurse | ForEach-Object -Process {Write-Host $_.name -foregroundcolor cyan}
+#List out the NuSpec files in the solutuon
+Get-ChildItem -Path "..\*\*.nuspec" -Recurse | ForEach-Object -Process 
+{Write-Host $_.name}
 
-Get-ChildItem -Path "..\*\*.nuspec" -Recurse | ForEach-Object -Process {
-    (Get-Content $_) -Replace '{UpdateVersion.ps1.replace}', $nugetversion | Set-Content $_
-}
-Write-Host "Over and out."
+#Update the NuSpec files with the correct version
+Get-ChildItem -Path "..\*\*.nuspec" -Recurse | ForEach-Object -Process 
+{(Get-Content $_) -Replace '{UpdateVersion.ps1.replace}', $nugetversion | Set-Content $_}
+
+Write-Host "All done here."
