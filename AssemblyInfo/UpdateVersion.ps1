b56bb7dd62c4e81f7d25f7cb2de3a7e418bc3aab
paulstancer <pstancer@hotmail.com>
2016-02-17 01:18:17 +0000

    Added the version Id hopefully.

--- AssemblyInfo/UpdateVersion.ps1
+++ AssemblyInfo/UpdateVersion.ps1
@@ -8,4 +8,7 @@ Write-Host "BUILD_SOURCESDIRECTORY contents:"
 gci $Env:BUILD_SOURCESDIRECTORY
 $url = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)$env:SYSTEM_TEAMPROJECTID/_apis/build/definitions/$($env:SYSTEM_DEFINITIONID)?api-version=2.0"
 Write-Host "URL: $url"
+Write-Host "BUILD_SOURCEVERSION contents:"
+gci $Env:BUILD_SOURCEVERSION
+
 Write-Host "Over and out."
