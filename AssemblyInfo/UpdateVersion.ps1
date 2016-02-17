c1c07a41dd6c0434903379d022ce49ea30ef6131
paulstancer <pstancer@hotmail.com>
2016-02-17 01:50:17 +0000

    Hoping for the best.

--- AssemblyInfo/UpdateVersion.ps1
+++ AssemblyInfo/UpdateVersion.ps1
@@ -1,3 +1,6 @@
 Write-Host "BUILD_BUILDNUMBER contents: $Env:BUILD_BUILDNUMBER"
 (Get-Content AssemblyInfo\SharedAssemblyInfo.cs).replace('0.0.0.0', $Env:BUILD_BUILDNUMBER) | Set-Content AssemblyInfo\SharedAssemblyInfo.cs
+Get-ChildItem "**\*.nuspec" -Recurse | ForEach-Object -Process {
+    (Get-Content $_) -Replace '{ps1.replace}', $Env:BUILD_BUILDNUMBER | Set-Content $_
+}
 Write-Host "Over and out."
