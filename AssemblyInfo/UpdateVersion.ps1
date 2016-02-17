cea75d1e8c76ff1d96ca4ae7b818366e19d679fa
paulstancer <pstancer@hotmail.com>
2016-02-17 09:43:28 +0000

    Update to powershell

--- AssemblyInfo/UpdateVersion.ps1
+++ AssemblyInfo/UpdateVersion.ps1
@@ -1,4 +1,9 @@
 Write-Host "BUILD_BUILDNUMBER contents: $Env:BUILD_BUILDNUMBER"
+Write-Host "Num Args:" $args.Length;
+foreach ($arg in $args)
+{
+  Write-Host "Arg: $arg";
+}
 (Get-Content AssemblyInfo\SharedAssemblyInfo.cs).replace('0.0.0.0', $Env:BUILD_BUILDNUMBER) | Set-Content AssemblyInfo\SharedAssemblyInfo.cs
 Get-ChildItem "**\*.nuspec" -Recurse | ForEach-Object -Process {
     (Get-Content $_) -Replace '{ps1.replace}', $Env:BUILD_BUILDNUMBER | Set-Content $_
