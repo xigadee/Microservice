1c0c4581c0df65b55d3516b51ad901e7d871acd1
paulstancer <pstancer@hotmail.com>
2016-02-17 10:43:28 +0000

    Update to Nuspec and to powershell to assign correct versions.

--- AssemblyInfo/UpdateVersion.ps1
+++ AssemblyInfo/UpdateVersion.ps1
@@ -1,11 +1,18 @@
-param([string]$version = "version", [string]$subversion = "sub")
+param(
+	[string]$version = $null, 
+	[string]$subversion = $null
+)
 Write-Host "BUILD_BUILDNUMBER contents: $Env:BUILD_BUILDNUMBER"
+
 IF([string]::IsNullOrWhitespace($version)){$version=$Env:BUILD_BUILDNUMBER}
-Write-Host "Version:" $version;
+Write-Host "Assembly Version:" $version;
+
 IF(![string]::IsNullOrWhitespace($subversion)){$nugetversion=$version+"-"+$subversion}
 Write-Host "Nuget Version:" $nugetversion;
+
 (Get-Content AssemblyInfo\SharedAssemblyInfo.cs).replace('0.0.0.0', $version) | Set-Content AssemblyInfo\SharedAssemblyInfo.cs
+
 Get-ChildItem "**\*.nuspec" -Recurse | ForEach-Object -Process {
-    (Get-Content $_) -Replace '{ps1.replace}', $nugetversion | Set-Content $_
+    (Get-Content $_) -Replace '$nugetversion', $nugetversion | Set-Content $_
 }
 Write-Host "Over and out."
