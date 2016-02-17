1f250759d8162a58fa1bc1b73ff8adfe757fb2a2
paulstancer <pstancer@hotmail.com>
2016-02-17 10:33:56 +0000

    Updated Powershell version checker.

--- AssemblyInfo/UpdateVersion.ps1
+++ AssemblyInfo/UpdateVersion.ps1
@@ -1,11 +1,11 @@
+param([string]$version = "version", [string]$subversion = "sub")
 Write-Host "BUILD_BUILDNUMBER contents: $Env:BUILD_BUILDNUMBER"
-Write-Host "Num Args:" $args.Length;
-foreach ($arg in $args)
-{
-  Write-Host "Arg: $arg";
-}
-(Get-Content AssemblyInfo\SharedAssemblyInfo.cs).replace('0.0.0.0', $Env:BUILD_BUILDNUMBER) | Set-Content AssemblyInfo\SharedAssemblyInfo.cs
+IF([string]::IsNullOrWhitespace($version)){$version=$Env:BUILD_BUILDNUMBER}
+Write-Host "Version:" $version;
+IF(![string]::IsNullOrWhitespace($subversion)){$nugetversion=$version+"-"+$subversion}
+Write-Host "Nuget Version:" $nugetversion;
+(Get-Content AssemblyInfo\SharedAssemblyInfo.cs).replace('0.0.0.0', $version) | Set-Content AssemblyInfo\SharedAssemblyInfo.cs
 Get-ChildItem "**\*.nuspec" -Recurse | ForEach-Object -Process {
-    (Get-Content $_) -Replace '{ps1.replace}', $Env:BUILD_BUILDNUMBER | Set-Content $_
+    (Get-Content $_) -Replace '{ps1.replace}', $nugetversion | Set-Content $_
 }
 Write-Host "Over and out."
