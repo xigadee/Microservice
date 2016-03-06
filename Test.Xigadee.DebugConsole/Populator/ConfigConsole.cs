1cbf9f8954ea85b9d6f5de09a6bbd09902692967
paulstancer <pstancer@hotmail.com>
2016-03-06 15:46:34 +0000

    Fixed missing file issue.

---
+++ Test.Xigadee.DebugConsole/Populator/ConfigConsole.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Xigadee;
+
+namespace Test.Xigadee
+{
+    /// <summary>
+    /// This is the shared configuration class for both Microservices.
+    /// </summary>
+    internal class ConfigConsole: ConfigBase
+    {
+        public string ServiceBusConnection => PlatformOrConfigCache("ServiceBusConnection");
+
+        public string RedisCacheName => PlatformOrConfigCache("RedisCacheName");
+        public string RedisCachePort => PlatformOrConfigCache("RedisCachePort", "6380");
+        public string RedisCacheKey => PlatformOrConfigCache("RedisCacheKey");
+
+        public string StorageName => PlatformOrConfigCache("StorageName");
+        public string StorageKey => PlatformOrConfigCache("StorageKey");
+
+        public string RedisCacheConnection
+        {
+            get
+            {
+                return $"{RedisCacheName}.redis.cache.windows.net:{RedisCachePort},ssl=true,password={RedisCacheKey}";
+            }
+        }
+
+        public StorageCredentials Storage
+        {
+            get
+            {
+                return new StorageCredentials(StorageName, StorageKey);
+            }
+        }
+
+        public DocumentDbConnection DocDbCredentials
+        {
+            get
+            {
+                return new DocumentDbConnection(DocumentDbName, DocumentDbKey);
+            }
+        }
+
+
+        public string DocumentDbName => PlatformOrConfigCache("DocumentDbName");
+
+        public string DocumentDbKey => PlatformOrConfigCache("DocumentDbKey");
+
+        public string DocumentDbDatabase => PlatformOrConfigCache("DocumentDbDatabase");
+
+    }
+}
+
+
