ab0f670d3dbafbb6546d494dd31754f42c43c671
paulstancer <pstancer@hotmail.com>
2016-04-19 15:37:27 +0100

    Added final two algorithms.

---
+++ Xigadee.Platform/Communication/ClientPriority/Algorithms/IListenerClientPollAlgorithm.cs
@@ -0,0 +1,39 @@
+﻿#region using
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+#endregion
+namespace Xigadee
+{
+    /// <summary>
+    /// This is the interface used by poll algorithms.
+    /// </summary>
+    public interface IListenerClientPollAlgorithm
+    {
+        /// <summary>
+        /// This is the number of allowed additional slots over the available slots.
+        /// </summary>
+        int AllowedOverage { get; set; }
+        /// <summary>
+        /// This is the priority recalculate frequency. Leave this null if you do not wish it to recalculate.
+        /// </summary>
+        TimeSpan? PriorityRecalculateFrequency { get; set; }
+
+        string Name { get; }
+
+        TimeSpan MaxAllowedPollWait { get; set; }
+
+        TimeSpan MinExpectedPollWait { get; set; }
+
+        decimal PollTimeReduceRatio { get; set; }
+
+        double CapacityPercentage { get; set; }
+
+    }
+
+
+}
--- Xigadee.Platform/Communication/ClientPriority/Algorithms/ListenerClientPollAlgorithmBase.cs
+++ Xigadee.Platform/Communication/ClientPriority/Algorithms/ListenerClientPollAlgorithmBase.cs
@@ -10,32 +10,6 @@ using System.Threading.Tasks;
 namespace Xigadee
 {
     /// <summary>
-    /// This is the interface used by poll algorithms.
-    /// </summary>
-    public interface IListenerClientPollAlgorithm
-    {
-        /// <summary>
-        /// This is the number of allowed additional slots over the available slots.
-        /// </summary>
-        int AllowedOverage { get; set; }
-        /// <summary>
-        /// This is the priority recalculate frequency. Leave this null if you do not wish it to recalculate.
-        /// </summary>
-        TimeSpan? PriorityRecalculateFrequency { get; set; }
-
-        string Name { get; }
-
-        TimeSpan MaxAllowedPollWait { get; set; } 
-
-        TimeSpan MinExpectedPollWait { get; set; }
-
-        decimal PollTimeReduceRatio { get; set; }
-
-        double CapacityPercentage { get; set; }
-
-    }
-
-    /// <summary>
     /// This abstract class is the base class to allocate polling slots to the listener collection.
     /// </summary>
     public abstract class ListenerClientPollAlgorithmBase: IListenerClientPollAlgorithm
