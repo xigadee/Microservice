eb42425b00a02023a6ede8cfd6726eedafadf7d7
paulstancer <pstancer@hotmail.com>
2016-07-11 17:12:28 +0100

    Small tweaks

---
+++ Test.Xigadee.Shared/Entities/ComplexEntity/ComplexEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Xigadee
+{
+    public class ComplexEntity
+    {
+        public ComplexEntity()
+        {
+            One = DateTime.Now.Second;
+            Two = Guid.NewGuid();
+            Message = DateTime.Now.ToString();
+        }
+
+        public int One { get; set; }
+
+        public Guid Two { get; set; }
+
+        public string Message { get; set; }
+
+        public ComplexKey ToKey()
+        {
+            return new ComplexKey() { One = One, Two = Two };
+        }
+    }
+}
---
+++ Test.Xigadee.Shared/Entities/ComplexEntity/ComplexKey.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xigadee;
+
+namespace Test.Xigadee
+{
+    [KeyMapper(typeof(ComplexKeyMapper))]
+    public class ComplexKey:IEquatable<ComplexKey>
+    {
+        public int One { get; set; }
+        public Guid Two { get; set; }
+
+        public bool Equals(ComplexKey other)
+        {
+            if (other == null)
+                return false;
+
+            return other.One == this.One && other.Two == this.Two;
+        }
+    }
+
+
+
+
+}
