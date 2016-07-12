aa7c1c751ba311a45c3b3ae1d0fee59337e279e9
paulstancer <pstancer@hotmail.com>
2016-07-12 22:46:30 +0100

    Addition of DocDb settings and minor refactoring.

---
+++ Xigadee.Core/Populator/ConfigBaseHelperDocumentDb.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xigadee
+{
+
+    public static class ConfigBaseHelperDocumentDb
+    {
+        public const string KeyDocDBAccountName = "DocDBAccountName";
+        public const string KeyDocDBAccountAccessKey = "DocDBAccountAccessKey";
+        public const string KeyDocDBDatabaseName = "DocDBDatabaseName";
+        public const string KeyDocDBCollectionName = "DocDBCollectionName";
+
+
+        public static DocumentDbConnection DocDBConnection(this ConfigBase config) => DocumentDbConnection.ToConnection(config.DocDBAccountName(), config.DocDBAccountAccessKey());
+
+        public static string DocDBAccountName(this ConfigBase config) => config.PlatformOrConfigCache(KeyDocDBAccountName);
+
+        public static string DocDBAccountAccessKey(this ConfigBase config) => config.PlatformOrConfigCache(KeyDocDBAccountAccessKey);
+
+        public static string DocDBDatabaseName(this ConfigBase config) => config.PlatformOrConfigCache(KeyDocDBDatabaseName);
+
+        public static string DocDBCollectionName(this ConfigBase config) => config.PlatformOrConfigCache(KeyDocDBCollectionName);
+    }
+}
--- Xigadee.Core/Populator/ConfigBaseHelperService.cs
+++ Xigadee.Core/Populator/ConfigBaseHelperService.cs
@@ -8,14 +8,16 @@ namespace Xigadee
 {
     public static class ConfigBaseHelperService
     {
+        public const string KeyEnvironment = "Environment";
+        public const string KeyClient = "Client";
+        public const string KeyServiceDisabled = "ServiceDisabled";
+
         public static bool ServiceDisabled(this ConfigBase config) => config.PlatformOrConfigCacheBool(KeyServiceDisabled);
 
         public static string Client(this ConfigBase config) => config.PlatformOrConfigCache(KeyClient);
 
         public static string Environment(this ConfigBase config) => config.PlatformOrConfigCache(KeyEnvironment);
 
-        public const string KeyEnvironment = "Environment";
-        public const string KeyClient = "Client";
-        public const string KeyServiceDisabled = "ServiceDisabled";
     }
+
 }
