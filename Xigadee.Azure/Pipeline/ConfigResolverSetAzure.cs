ddc13c339437866e2f828c1035887f4376d2d581
paulstancer <pstancer@hotmail.com>
2016-11-15 17:38:29 +0000

    Added azure cloud to the config pipeline.

---
+++ Xigadee.Azure/Pipeline/ConfigResolverSetAzure.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xigadee
+{
+    /// <summary>
+    /// These extension methods connect the service bus listeners in to the pipeline.
+    /// </summary>
+    public static partial class AzureExtensionMethods
+    {
+        /// <summary>
+        /// This extension method sets the service to use the CloudConfigurationManager at priority 20 by default.
+        /// </summary>
+        /// <typeparam name="P">The pipeline type.</typeparam>
+        /// <param name="pipeline">The pipeline</param>
+        /// <param name="priority">The optional priority, by default set to 20.</param>
+        /// <param name="assign">An action to allow changes to be made to the resolver after it is created.</param>
+        /// <returns>Returns the pipeline to continue the chain.</returns>
+        public static P ConfigResolverSetAzure<P>(this P pipeline, int priority = 20, Action<ConfigResolverAzure> assign = null)
+        where P : MicroservicePipeline
+        {
+            pipeline.ConfigResolverSet<P, ConfigResolverAzure>(priority, assign);
+
+            return pipeline;
+        }
+    }
+}
