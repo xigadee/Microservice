d6b55f573f4bc57d11ade45cea8fbcc89d53c7f5
Guy Steel <guy.steel@imgroup.com>
2016-11-29 18:33:11 +0000

    Include optional additional data to be passed alongside metric events to allow for metric data slicing

--- Xigadee.Platform/DataCollection/Events/MetricEvent.cs
+++ Xigadee.Platform/DataCollection/Events/MetricEvent.cs
@@ -10,8 +10,19 @@ namespace Xigadee
     [DebuggerDisplay("Metric={MetricName}/{Value}")]
     public class MetricEvent: EventBase
     {
+        /// <summary>
+        /// Name
+        /// </summary>
         public string MetricName { get; set; }
 
+        /// <summary>
+        /// Value
+        /// </summary>
         public double Value { get; set; }
+
+        /// <summary>
+        /// Addition data to associate when logging the metric
+        /// </summary>
+        public virtual Dictionary<string, string> AdditionalData { get; } = new Dictionary<string, string>();
     }
 }
