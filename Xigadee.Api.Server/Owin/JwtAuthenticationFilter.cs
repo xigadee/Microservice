1c255e92ef9066644d05d89aa286f1dcc1a329a0
Guy Steel <guy.steel@imgroup.com>
2017-02-23 15:22:19 +0000

    Add requires https filter

--- Xigadee.Api.Server/Owin/JwtAuthenticationFilter.cs
+++ Xigadee.Api.Server/Owin/JwtAuthenticationFilter.cs
@@ -80,37 +80,4 @@ namespace Xigadee
             return Task.CompletedTask;
         }
     }
-
-    /// <summary>
-    /// This class is used to assign the status.
-    /// </summary>
-    public class StatusResult: IHttpActionResult
-    {
-        public StatusResult(HttpStatusCode status, HttpRequestMessage request, string reasonPhrase = null)
-        {
-            Status = status;
-            ReasonPhrase = reasonPhrase;
-            Request = request;
-        }
-
-        public HttpStatusCode Status { get; private set; }
-
-        public string ReasonPhrase { get; private set; }
-
-        public HttpRequestMessage Request { get; private set; }
-
-        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
-        {
-            return Task.FromResult(Execute());
-        }
-
-        private HttpResponseMessage Execute()
-        {
-            HttpResponseMessage response = new HttpResponseMessage(Status);
-            response.RequestMessage = Request;
-            response.ReasonPhrase = ReasonPhrase ?? Enum.GetName(typeof(HttpStatusCode), Status);
-            return response;
-        }
-    }
-
 }
---
+++ Xigadee.Api.Server/Pipeline/ApiAdd/ApiAddRequiresHttpsFilter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Filters;
+
+namespace Xigadee
+{
+    public static partial class WebApiExtensionMethods
+    {
+        /// <summary>
+        /// Adds a requires https filter to the pipeline - prevents non https traffic accessing the api
+        /// </summary>
+        /// <typeparam name="P"></typeparam>
+        /// <param name="webpipe"></param>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public static P ApiAddRequiresHttps<P>(this P webpipe, Action<IAuthenticationFilter> action = null) where P : IPipelineWebApi
+        {
+            var filter = new RequireHttpsFilter();
+            action?.Invoke(filter);
+            webpipe.HttpConfig.Filters.Add(filter);
+            return webpipe;
+        }
+    }
+}
