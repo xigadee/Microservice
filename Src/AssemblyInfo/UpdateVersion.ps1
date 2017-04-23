a5153546c949402d641c7cfe421fe7a6e9fa30fc
paulstancer <pstancer@hotmail.com>
2017-04-23 11:50:05 +0100

    Fix for NuSpec file path

--- Src/AssemblyInfo/UpdateVersion.ps1
+++ Src/AssemblyInfo/UpdateVersion.ps1
@@ -23,8 +23,8 @@ IF(![string]::IsNullOrWhitespace($subversion)){$nugetversion=$version+"-"+$subve
 Write-Host "Nuget Version:" $nugetversion;
 
 #Set the Assembly versions for the DLLs.
-(Get-Content AssemblyInfo\SharedAssemblyInfo.cs).replace('AssemblyInformationalVersion("0.0.0.0")', 'AssemblyInformationalVersion("'+$nugetversion+'")') | Set-Content AssemblyInfo\SharedAssemblyInfo.cs
-(Get-Content AssemblyInfo\SharedAssemblyInfo.cs).replace('0.0.0.0', $version) | Set-Content AssemblyInfo\SharedAssemblyInfo.cs
+(Get-Content Src\AssemblyInfo\SharedAssemblyInfo.cs).replace('AssemblyInformationalVersion("0.0.0.0")', 'AssemblyInformationalVersion("'+$nugetversion+'")') | Set-Content Src\AssemblyInfo\SharedAssemblyInfo.cs
+(Get-Content Src\AssemblyInfo\SharedAssemblyInfo.cs).replace('0.0.0.0', $version) | Set-Content Src\AssemblyInfo\SharedAssemblyInfo.cs
 
 #List out the NuSpec files in the solutuon
 Get-ChildItem -Path "..\*\*.nuspec" -Recurse | ForEach-Object -Process {
