b538627f98131b8f07879ba62abef32d8106df96
Guy Steel <guy.steel@imgroup.com>
2017-05-11 19:27:42 +0100

    EventHubs Data Collector Implementation

--- Src/Xigadee.Azure/DataCollection/EventHubs/EventHubsDataCollectorOptions.cs
+++ Src/Xigadee.Azure/DataCollection/EventHubs/EventHubsDataCollectorOptions.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.WindowsAzure.Storage.Table;
 
 namespace Xigadee
 {
@@ -16,32 +11,37 @@ namespace Xigadee
         /// This is the default constructor.
         /// </summary>
         /// <param name="support">The collection support type.</param>
+        /// <param name="serializerBinary">Serializer used to serialize the event object</param>
+        /// <param name="entityPath">Function to determine the entity path that will be used when sending messages to event hub</param>
+        /// <param name="isSupported">Function to determine whether the event is supported</param>
         public EventHubDataCollectorOptions(DataCollectionSupport support
-            )
+            , Func<EventHolder, MicroserviceId, AzureStorageBinary> serializerBinary = null
+            , Func<EventHolder, MicroserviceId, string> entityPath = null
+            , Func<EventHolder, bool> isSupported = null)
         {
             Support = support;
 
+            SerializerBinary = serializerBinary ?? AzureStorageHelper.DefaultJsonBinarySerializer;
+            EntityPath = entityPath ?? ((ev, ms) => $"{ms.Name}_{support.ToString()}");
+
+            IsSupported = isSupported ?? (ev => true);
         }
 
-        public Func<EventHolder, MicroserviceId, string> MakeId { get; set; }
-
-        public Func<EventHolder, MicroserviceId, string> BinaryMakeId { get; set; }
-
-        public Func<EventHolder, MicroserviceId, string> BinaryMakeFolder { get; set; }
-
         /// <summary>
-        /// This function can be set to provide specific table serialization.
+        /// This function can be set to provide the event hub entity patch
         /// </summary>
-        public Func<EventHolder, MicroserviceId, ITableEntity> SerializerTable { get; set; }
+        public Func<EventHolder, MicroserviceId, string> EntityPath { get; set; }
 
         /// <summary>
         /// This function can be set to provide specific binary serialization.
         /// </summary>
         public Func<EventHolder, MicroserviceId, AzureStorageBinary> SerializerBinary { get; set; }
+        
         /// <summary>
-        /// This function can be used to filter out specific event from writing for certain storage support.
+        /// This function can be used to filter out specific event from being written.
         /// </summary>
-        public Func<AzureStorageBehaviour, EventHolder, bool> IsSupported { get; set; }
+        public Func<EventHolder, bool> IsSupported { get; set; }
+
         /// <summary>
         /// This is the support type for the options handler, i.e. LogEvent, EventSource, etc.
         /// </summary>
@@ -55,12 +55,11 @@ namespace Xigadee
         /// <summary>
         /// The encryption storage policy.
         /// </summary>
-        public AzureStorageEncryption EncryptionPolicy { get; set; } = AzureStorageEncryption.BlobWhenPresent;
+        public AzureStorageEncryption EncryptionPolicy { get; set; } = AzureStorageEncryption.None;
 
         /// <summary>
         /// Specifies whether the profiler should be used for the write action.
         /// </summary>
         public bool ShouldProfile { get; set; } = true;
-
     }
 }
