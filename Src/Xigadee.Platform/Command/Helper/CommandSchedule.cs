9f47dbb537bfbc8e35bf8e87a4970a9ee945723e
paulstancer <pstancer@hotmail.com>
2017-06-06 12:29:52 +0100

    Minor tidyup of the code.

--- Src/Xigadee.Platform/Command/Helper/CommandSchedule.cs
+++ Src/Xigadee.Platform/Command/Helper/CommandSchedule.cs
@@ -21,30 +21,6 @@ using System.Threading.Tasks;
 namespace Xigadee
 {
     /// <summary>
-    /// This schedule class is used to record the timeout count statistics.
-    /// </summary>
-    public class CommandTimeoutSchedule: CommandSchedule
-    {
-        private long mTimeouts;
-
-        public CommandTimeoutSchedule(Func<Schedule, CancellationToken, Task> execute, CommandTimerPoll timerConfig, string name = null)
-            : base(execute, timerConfig, name)
-        {
-        }
-
-
-        public long Timeouts
-        {
-            get { return mTimeouts; }
-        }
-
-        public void TimeoutIncrement(long timeouts)
-        {
-            Interlocked.Add(ref mTimeouts, timeouts);
-        }
-    }
-
-    /// <summary>
     /// This is the command schedule generated for the specific command requirement.
     /// </summary>
     public class CommandSchedule: Schedule
@@ -59,10 +35,10 @@ namespace Xigadee
         public CommandSchedule(Func<Schedule, CancellationToken, Task> execute, CommandTimerPoll timerConfig, string name = null, bool isLongRunning = false)
             : base(execute, name)
         {
-            base.Frequency = timerConfig.Interval;
-            base.InitialTime = timerConfig.InitialWaitUTCTime;
-            base.InitialWait = timerConfig.InitialWait;
-            base.IsLongRunning = isLongRunning;
+            Frequency = timerConfig.Interval;
+            InitialTime = timerConfig.InitialWaitUTCTime;
+            InitialWait = timerConfig.InitialWait;
+            IsLongRunning = isLongRunning;
         }
     }
 }
---
+++ Src/Xigadee.Platform/Command/Helper/CommandTimeoutSchedule.cs
@@ -0,0 +1,56 @@
+﻿#region Copyright
+// Copyright Hitachi Consulting
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//    http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Xigadee
+{
+    /// <summary>
+    /// This schedule class is used to record the timeout count statistics.
+    /// </summary>
+    public class CommandTimeoutSchedule: CommandSchedule
+    {
+        private long mTimeouts;
+        /// <summary>
+        /// This is the default constructor.
+        /// </summary>
+        /// <param name="execute">The function to execute.</param>
+        /// <param name="timerConfig">The timerconfig job.</param>
+        /// <param name="name">The name.</param>
+        public CommandTimeoutSchedule(Func<Schedule, CancellationToken, Task> execute, CommandTimerPoll timerConfig, string name = null)
+            : base(execute, timerConfig, name)
+        {
+        }
+
+        /// <summary>
+        /// This is the total count of the number of timeouts.
+        /// </summary>
+        public long Timeouts
+        {
+            get { return mTimeouts; }
+        }
+        /// <summary>
+        /// This method increments the number of timeouts atomically.
+        /// </summary>
+        /// <param name="timeouts"></param>
+        public void TimeoutIncrement(long timeouts)
+        {
+            Interlocked.Add(ref mTimeouts, timeouts);
+        }
+    }
+}
