99bfa98b39f6fa48f8c91ea3a80b7938e0208c13
Paul Stancer <pstancer@hotmail.com>
2017-09-21 04:05:20 +0100

    Minor removal of test projects

--- Src/Test.Xigadee.Sample.HiC/UnitTest1.cs
+++ Src/Test.Xigadee.Sample.HiC/UnitTest1.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Xigadee.Sample.HiC;
+﻿//using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Xigadee.Sample.HiC;
 
-namespace Test.Xigadee.Sample
-{
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            ClassParse.DoSomething();
-        }
-    }
-}
+//namespace Test.Xigadee.Sample
+//{
+//    [TestClass]
+//    public class UnitTest1
+//    {
+//        [TestMethod]
+//        public void TestMethod1()
+//        {
+//            ClassParse.DoSomething();
+//        }
+//    }
+//}
