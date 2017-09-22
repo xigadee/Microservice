a5aad5a4e545a2615e1d25cef465f78ca934191b
Paul Stancer <pstancer@hotmail.com>
2017-09-22 10:20:04 +0100

    Fixed strange issue with DocumentDB client. Seems we have to use the core project as the other NuGet package does not support .NET Standard 2.0

---
+++ Src/Xigadee.Azure.DocumentDB/Persistence/SDKHelper.cs
@@ -0,0 +1,195 @@
+﻿#region Copyright
+// Copyright Hitachi Consulting
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//    http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+namespace Xigadee
+{
+    /// <summary>
+    /// This is the extension class to give CRUD based support to the DocumentClient.
+    /// </summary>
+    public static class DocumentDbSDKHelper
+    {
+        /// <summary>
+        /// Returns a Direct TCP DocumentClient from a connection.
+        /// </summary>
+        /// <param name="conn">The documentDB connection.</param>
+        /// <param name="policy">The policy.</param>
+        /// <param name="level">The consistency level.></param>
+        /// <returns></returns>
+        public static DocumentClient ToDocumentClient(this DocumentDbConnection conn, ConnectionPolicy policy = null, ConsistencyLevel? level = null)
+        {
+            return new DocumentClient(conn.Account, conn.AccountKey, policy?? new ConnectionPolicy
+            {
+                ConnectionMode = ConnectionMode.Direct,
+                ConnectionProtocol = Protocol.Tcp
+            }, level);
+        }
+
+        /// <summary>
+        /// This extension method creates an entity
+        /// </summary>
+        /// <param name="client">The documentDB client</param>
+        /// <param name="json">The json payload.</param>
+        /// <param name="nameDatabase">The db name.</param>
+        /// <param name="nameCollection">The collection name.</param>
+        /// <returns>Returns a response holder with the result.</returns>
+        public static async Task<ResponseHolder> CreateGeneric(this DocumentClient client, string json, string nameDatabase, string nameCollection)
+        {
+            return await DocDbExceptionWrapper(async e =>
+            {
+
+                ResourceResponse<Document> resultDocDb = null;
+                using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(json)))
+                {
+                    var doc = Document.LoadFrom<Document>(ms);
+                    var uri = UriFactory.CreateDocumentCollectionUri(nameDatabase, nameCollection);
+                    resultDocDb = await client.CreateDocumentAsync(uri, doc, disableAutomaticIdGeneration: true);
+                }
+
+                e.IsSuccess = true;
+                e.StatusCode = (int)resultDocDb.StatusCode;
+                e.Content = resultDocDb.Resource.ToString();
+                e.ETag = resultDocDb.Resource.ETag;
+                e.ResourceCharge = resultDocDb.RequestCharge;
+            }
+            );
+        }
+        /// <summary>
+        /// This extension method reads an entity.
+        /// </summary>
+        /// <param name="client">The documentDB client</param>
+        /// <param name="nameDatabase">The db name.</param>
+        /// <param name="nameCollection">The collection name.</param>
+        /// <param name="nameId">The item id.</param>
+        /// <returns>Returns a response holder with the result and the entity.</returns>
+        public static async Task<ResponseHolder> ReadGeneric(this DocumentClient client, string nameDatabase, string nameCollection, string nameId)
+        {
+            return await DocDbExceptionWrapper(async e =>
+            {
+                var uri = UriFactory.CreateDocumentUri(nameDatabase, nameCollection, nameId);
+
+                ResourceResponse<Document> resultDocDb = await client.ReadDocumentAsync(uri);
+
+                e.IsSuccess = true;
+                e.StatusCode = (int)resultDocDb.StatusCode;
+                e.Content = resultDocDb.Resource.ToString();
+                e.ETag = resultDocDb.Resource.ETag;
+                e.ResourceCharge = resultDocDb.RequestCharge;
+            });
+        }
+        /// <summary>
+        /// This extension method updates an existing entity.
+        /// </summary>
+        /// <param name="client">The documentDB client</param>
+        /// <param name="json">The json payload.</param>
+        /// <param name="nameDatabase">The db name.</param>
+        /// <param name="nameCollection">The collection name.</param>
+        /// <param name="eTag">The entity etag.</param>
+        /// <returns>Returns a response holder with the result.</returns>
+        public static async Task<ResponseHolder> UpdateGeneric(this DocumentClient client, string json, string nameDatabase, string nameCollection, string eTag = null)
+        {
+            return await DocDbExceptionWrapper(async e =>
+            {
+                ResourceResponse<Document> resultDocDb = null;
+
+                using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(json)))
+                {
+                    var doc = Document.LoadFrom<Document>(ms);
+                    var uri = UriFactory.CreateDocumentCollectionUri(nameDatabase, nameCollection);
+                    resultDocDb = await client.UpsertDocumentAsync(uri, doc, disableAutomaticIdGeneration: true, options: ETagOptions(eTag));
+
+                }
+
+                e.IsSuccess = true;
+                e.StatusCode = (int)resultDocDb.StatusCode;
+                e.Content = resultDocDb.Resource.ToString();
+                e.ETag = resultDocDb.Resource.ETag;
+                e.ResourceCharge = resultDocDb.RequestCharge;
+            }
+            );
+        }
+
+        private static RequestOptions ETagOptions(string eTag)
+        {
+            if (eTag == null)
+                return null;
+
+
+            var condition = new AccessCondition();
+            condition.Type = AccessConditionType.IfMatch;
+            condition.Condition = eTag;
+
+            return new RequestOptions() { AccessCondition = condition };
+        }
+        /// <summary>
+        /// This is the generic delete override.
+        /// </summary>
+        /// <param name="client">The documentDB client</param>
+        /// <param name="nameDatabase">The db name.</param>
+        /// <param name="nameCollection">The collection name.</param>
+        /// <param name="nameId">The item id.</param>
+        /// <returns>Returns a response holder with the result.</returns>
+        public static async Task<ResponseHolder> DeleteGeneric(this DocumentClient client, string nameDatabase, string nameCollection, string nameId)
+        {
+            return await DocDbExceptionWrapper(async e =>
+            {
+                var uri = UriFactory.CreateDocumentUri(nameDatabase, nameCollection, nameId);
+
+                ResourceResponse<Document> resultDocDb = await client.DeleteDocumentAsync(uri);
+
+                e.IsSuccess = true;
+                e.StatusCode = (int)resultDocDb.StatusCode;
+                e.ResourceCharge = resultDocDb.RequestCharge;
+            });
+        }
+
+        private static async Task<ResponseHolder> DocDbExceptionWrapper(Func<ResponseHolder, Task> action)
+        {
+            var response = new ResponseHolder();
+
+            try
+            {
+                await action(response);
+            }
+            catch (DocumentClientException dex)
+            {
+                response.StatusCode = (int)dex.StatusCode;
+                response.IsSuccess = false;
+
+                if (response.StatusCode == 429)
+                {
+                    response.ThrottleHasWaited = true;
+                    response.ThrottleSuggestedWait = dex.RetryAfter;
+                }
+
+                response.ResourceCharge = dex.RequestCharge;
+            }
+            catch (Exception ex)
+            {
+                response.Ex = ex;
+                response.StatusCode = 500;
+                response.IsSuccess = false;
+            }
+
+            return response;
+        }
+    }
+}
