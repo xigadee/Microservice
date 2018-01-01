e3183ea3fef8355537a008a99d37f56651809a4a
Paul Stancer <pstancer@hotmail.com>
2018-01-01 10:26:35 -0800

    Minor change to documentation.

--- Src/Xigadee.Platform/Pipeline/Extensions/Revert.cs
+++ Src/Xigadee.Platform/Pipeline/Extensions/Revert.cs
@@ -1,35 +1,13 @@
-﻿#region Copyright
-// Copyright Hitachi Consulting
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//    http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-#endregion
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Xigadee
+﻿namespace Xigadee
 {
     public static partial class CorePipelineExtensions
     {
         /// <summary>
-        /// This method reverts a pipeline extension to the underlying pipeline.
+        /// This method reverts a pipeline extension to the parent pipeline.
         /// </summary>
         /// <typeparam name="P">The pipeline extension type.</typeparam>
         /// <param name="cpipe">The pipeline extension.</param>
-        /// <returns>The underlying Microservice extension.</returns>
+        /// <returns>The parent Microservice pipeline.</returns>
         public static P Revert<P>(this IPipelineExtension<P> cpipe)
             where P : class, IPipeline
         {
