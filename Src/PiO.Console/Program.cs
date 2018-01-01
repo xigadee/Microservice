88907a35516c066ccac1b7e56cf1cfdfb61d873c
Paul Stancer <pstancer@hotmail.com>
2018-01-01 10:17:13 -0800

    Udp test code added and tidy up.

--- Src/PiO.Console/Program.cs
+++ Src/PiO.Console/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using Xigadee;
 
@@ -9,8 +11,77 @@ namespace PiO
     {
         static MicroservicePipeline mservice;
 
+        static void UdpTransmitHelper(string mcastGroup, int port, int ttl, string rep)
+        {
+            IPAddress ip;
+            try
+            {
+                Console.WriteLine("MCAST Send on Group: {0} Port: {1} TTL: {2}", mcastGroup, port, ttl);
+                ip = IPAddress.Parse(mcastGroup);
+
+                Socket s = new Socket(AddressFamily.InterNetwork,
+                                SocketType.Dgram, ProtocolType.Udp);
+
+                s.SetSocketOption(SocketOptionLevel.IP,
+                    SocketOptionName.AddMembership, new MulticastOption(ip));
+
+                s.SetSocketOption(SocketOptionLevel.IP,
+                    SocketOptionName.MulticastTimeToLive, ttl);
+
+                byte[] b = new byte[10];
+                for (int x = 0; x < b.Length; x++) b[x] = (byte)(x + 65);
+
+                IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(mcastGroup), port);
+
+                Console.WriteLine("Connecting...");
+
+                s.Connect(ipep);
+
+                for (int x = 0; x < int.Parse(rep); x++)
+                {
+                    Console.WriteLine("Sending ABCDEFGHIJ...");
+                    s.Send(b, b.Length, SocketFlags.None);
+                }
+
+                Console.WriteLine("Closing Connection...");
+                s.Close();
+            }
+            catch (System.Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+            }
+        }
+
+        static void UdpReceiveHelper(string mcastGroup, int port)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, port);
+            s.Bind(ipep);
+
+            IPAddress ip = IPAddress.Parse(mcastGroup);
+
+            s.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(ip, IPAddress.Any));
+
+            while (true)
+            {
+                byte[] b = new byte[10];
+                Console.WriteLine("Waiting for data..");
+                s.Receive(b);
+                string str = System.Text.Encoding.ASCII.GetString(b, 0, b.Length);
+                Console.WriteLine("RX: " + str.Trim());
+            }
+            //s.Close();
+        }
+
         static void Main(string[] args)
         {
+            var name = Dns.GetHostName();
+            var host = Dns.GetHostEntry(name);
+            var listv4 = host.AddressList.Where((ip) => ip.AddressFamily == AddressFamily.InterNetwork).ToList();
+            var listv6 = host.AddressList.Where((ip) => ip.AddressFamily == AddressFamily.InterNetworkV6).ToList();
+
+
             DebugMemoryDataCollector coll;
             mservice = new MicroservicePipeline("PiO", description: "PiO Server");
             mservice
