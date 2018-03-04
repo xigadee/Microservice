6d255b9a356774b18d2951bdd492758f7f4a7e1d
paulstancer <pstancer@hotmail.com>
2018-03-04 15:16:51 +0000

    Update to NuSpec files

--- Src/AssemblyInfo/SharedAssemblyInfo.cs
+++ Src/AssemblyInfo/SharedAssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Reflection;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyCompany("Xigadee")]
-[assembly: AssemblyCopyright("Copyright (c) Xigadee 2017-2018 with parts are Hitachi Consulting 2012-2017")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 
 [assembly: AssemblyVersion("0.0.0.0")]
