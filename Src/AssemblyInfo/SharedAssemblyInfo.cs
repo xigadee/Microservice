3c150b291c2305e2941231e965e8b0ca3eab3001
paulstancer <pstancer@hotmail.com>
2018-03-06 00:20:21 +0000

    Fixed small mistake in Azure Storage NuSpec file

--- Src/AssemblyInfo/SharedAssemblyInfo.cs
+++ Src/AssemblyInfo/SharedAssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Xigadee")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 
