500570c12831b99ede5878377228f342f0f6f500
paulstancer <pstancer@hotmail.com>
2018-04-01 14:08:13 +0100

    Refactored the abstractions library to support based definitions. Ready for test in NuGet.

---
+++ Src/Xigadee.Abstractions/Configuration/ConfigResolver/ConfigResolverFunction.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Xigadee
+{
+    /// <summary>
+    /// This is the function resolver.
+    /// </summary>
+    public class ConfigResolverFunction: ConfigResolver
+    {
+        private Func<string, string, string> mResolver;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigResolverFunction"/> class.
+        /// </summary>
+        /// <param name="resolver">The resolver.</param>
+        /// <exception cref="System.ArgumentNullException">resolver cannot be null</exception>
+        public ConfigResolverFunction(Func<string, string, string> resolver)
+        {
+            if (resolver == null)
+                throw new ArgumentNullException("resolver cannot be null");
+            mResolver = resolver;
+        }
+        /// <summary>
+        /// Use this method to get the value from the resolver.
+        /// </summary>
+        /// <param name="key">The key to resolve</param>
+        /// <returns>
+        /// Returns true if it can resolve.
+        /// </returns>
+        public override bool CanResolve(string key)
+        {
+            return mResolver(key, null) != null;
+        }
+        /// <summary>
+        /// Use this method to get the value from the resolver.
+        /// </summary>
+        /// <param name="key">The key to resolve</param>
+        /// <returns>
+        /// This is the settings value, null if not set.
+        /// </returns>
+        public override string Resolve(string key)
+        {
+            return mResolver(key, null);
+        }
+    }
+}
---
+++ Src/Xigadee.Abstractions/_Docs/Introduction.md
@@ -0,0 +1,8 @@
+﻿<table>
+<tr>
+<td width="80%"><a href="../../../README.md"><img src="../../../docs/X2a.png" alt="Xigadee"></a></td>
+<td width = "*" align="right"><img src="../../../docs/smallWIP.jpg" alt="Sorry, I'm still working here" height="100"></td>
+</tr>
+</table>
+
+# Xigadee.Abstractions
