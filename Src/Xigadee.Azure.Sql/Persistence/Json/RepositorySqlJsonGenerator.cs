00018feb37151ce55f54161d43643cdee18d73ca
paulstancer <pstancer@hotmail.com>
2019-04-05 17:31:18 +0100

    Minor change to the generator.

---
+++ Src/Xigadee.Azure.Sql/Persistence/Json/RepositorySqlJsonGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xigadee
+{
+    /// <summary>
+    /// This class can be used to generate the SQL Schema for the JSON based tables and stored procedures.
+    /// </summary>
+    /// <typeparam name="E">The entity type.</typeparam>
+    public class RepositorySqlJsonGenerator<E>
+    {
+        private SqlStoredProcedureResolver<E> _resolver;
+
+        public RepositorySqlJsonGenerator(SqlStoredProcedureResolver<E> resolver)
+        {
+            _resolver = resolver;
+
+        }
+    }
+}
