f2ab3b3f51a75e21ec5c60f5e55907d895053d3c
paulstancer <pstancer@hotmail.com>
2019-04-13 08:21:20 -0700

    Minor change to refactored code.

--- Src/Xigadee.Abstractions/Persistence/Command/Client/Pipeline/AttachPersistenceClient.cs
+++ Src/Xigadee.Abstractions/Persistence/Command/Client/Pipeline/AttachPersistenceClient.cs
@@ -4,6 +4,7 @@ namespace Xigadee
 {
     public static partial class CorePipelineExtensionsCore
     {
+        #region AttachPersistenceClient<C,K,E> ...
         /// <summary>
         /// This pipeline command creates a Persistence Message Initiator.
         /// </summary>
@@ -19,47 +20,29 @@ namespace Xigadee
         /// <param name="routing">The route map will attempt to first route the message internally and then try an external channel.</param>
         /// <param name="adjustPolicy">This method allows for the policy to be adjusted.</param>
         /// <returns>The pass through for the channel.</returns>
-        public static C AttachPersistenceClient<C,K,E>(this C cpipe
-            , out PersistenceClient<K,E> command
+        public static C AttachPersistenceClient<C, K, E>(this C cpipe
+            , out PersistenceClient<K, E> command
             , string responseChannel = null
             , int startupPriority = 90
-            , ICacheManager<K,E> cacheManager = null
+            , ICacheManager<K, E> cacheManager = null
             , TimeSpan? defaultRequestTimespan = null
             , ProcessOptions routing = ProcessOptions.RouteExternal | ProcessOptions.RouteInternal
             , Action<PersistenceClientPolicy> adjustPolicy = null
             )
             where C : IPipelineChannelIncoming<IPipeline>
             where K : IEquatable<K>
-        {        
+        {
             command = new PersistenceClient<K, E>(cacheManager, defaultRequestTimespan);
 
             adjustPolicy?.Invoke(command.Policy);
 
-            bool channelInternalOnly = responseChannel == null;
-            if (channelInternalOnly)
-                responseChannel = $"PersistenceClient{command.ComponentId.ToString("N").ToUpperInvariant()}";
-
-            if (!cpipe.ToMicroservice().Communication.HasChannel(responseChannel, ChannelDirection.Incoming))
-            {
-                var outPipe = cpipe.ToPipeline().AddChannelIncoming(responseChannel, isAutocreated:true, internalOnly: channelInternalOnly, description:$"Persistence Client Response: {typeof(E).Name}");
-                command.ResponseChannelId = outPipe.Channel.Id;
-            }
-            else
-                //If the response channel is not set, dynamically create a response channel.
-                command.ResponseChannelId = responseChannel;
-
-            //Check that the response channel exists
-            if (!cpipe.ToMicroservice().Communication.HasChannel(command.ResponseChannelId, ChannelDirection.Incoming))
-                throw new ChannelDoesNotExistException(command.ResponseChannelId, ChannelDirection.Incoming, cpipe.ToMicroservice().Id.Name);
-
-            command.ChannelId = cpipe.Channel.Id;
-            command.RoutingDefault = routing;
-
-            cpipe.Pipeline.AddCommand(command, startupPriority);
-
-            return cpipe;
-        }
-
+            return cpipe.SetPersistenceClientChannel(command
+                , responseChannel
+                , startupPriority
+                , routing);
+        } 
+        #endregion
+        #region SetPersistenceClientChannel<C> ...
         /// <summary>
         /// This method sets the routing parameters for the client.
         /// </summary>
@@ -69,7 +52,7 @@ namespace Xigadee
         /// <param name="responseChannel">The channel to send the response message on. If this is not set, a default response channel will be created.</param>
         /// <param name="startupPriority">The command start up priority. The default is 90.</param>
         /// <param name="routing">The route map will attempt to first route the message internally and then try an external channel.</param>
-        public static void SetPersistenceClientChannel<C>(this C cpipe, IPersistenceClientCommand command
+        public static C SetPersistenceClientChannel<C>(this C cpipe, IPersistenceClientCommand command
             , string responseChannel = null
             , int startupPriority = 90
             , ProcessOptions routing = ProcessOptions.RouteExternal | ProcessOptions.RouteInternal
@@ -97,7 +80,10 @@ namespace Xigadee
             command.RoutingDefault = routing;
 
             cpipe.Pipeline.AddCommand(command, startupPriority);
-        }
+
+            return cpipe;
+        } 
+        #endregion
 
         /// <summary>
         /// This pipeline command creates a Persistence Message Initiator.
@@ -126,12 +112,12 @@ namespace Xigadee
             where C : IPipelineChannelOutgoing<IPipeline>
             where K : IEquatable<K>
         {
+            if (responseChannel == null)
+                throw new ArgumentNullException("responseChannel", $"{nameof(AttachPersistenceClient)}: responseChannel cannot be null.");
+
             command = new PersistenceClient<K, E>(cacheManager, defaultRequestTimespan);
 
             adjustPolicy?.Invoke(command.Policy);
-
-            if (responseChannel == null)
-                throw new ArgumentNullException("responseChannel", $"{nameof(AttachPersistenceClient)}: responseChannel cannot be null.");
 
             //Check that the response channel exists
             if (verifyChannel && !cpipe.ToMicroservice().Communication.HasChannel(responseChannel, ChannelDirection.Incoming))
