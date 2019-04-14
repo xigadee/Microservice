0deb07714228128d6fd7956d1f46d2220e6af25d
paulstancer <pstancer@hotmail.com>
2019-04-13 22:39:23 -0700

    Added entity history.

---
+++ Src/Xigadee.Azure.Sql/Persistence/Json/SqlTemplates/StoredProcedures/spHistory.sql
@@ -0,0 +1,45 @@
+﻿CREATE PROCEDURE [{NamespaceTable}].[{spHistory}]
+	 @EntityId BIGINT
+	,@ExternalId UNIQUEIDENTIFIER
+	,@VersionId UNIQUEIDENTIFIER 
+	,@VersionIdNew UNIQUEIDENTIFIER 
+    ,@UserIdAudit UNIQUEIDENTIFIER 
+	,@Body NVARCHAR (MAX)
+	,@DateCreated DATETIME 
+	,@DateUpdated DATETIME 
+	,@Sig VARCHAR(256) 
+AS
+	BEGIN TRY
+
+		--Process the reference first.
+		INSERT INTO [{NamespaceTable}].[{EntityName}History] 
+		(
+			 [EntityId]
+			,[ExternalId] 
+			,[VersionId] 
+			,[UserIdAudit] 
+			,[DateCreated] 
+			,[DateUpdated] 
+			,[Sig] 
+			,[Body]
+		)
+		VALUES
+		(
+			 @EntityId 
+			,@ExternalId 
+			,@VersionIdNew  
+			,@UserIdAudit  
+			,@DateCreated  
+			,@DateUpdated  
+			,@Sig 
+			,@Body 
+		)
+
+		RETURN 200;
+	END TRY
+	BEGIN CATCH
+		IF (ERROR_NUMBER() = 2601)
+			RETURN 409; --Conflict - duplicate reference key to other transaction.
+		ELSE
+			THROW;
+	END CATCH
