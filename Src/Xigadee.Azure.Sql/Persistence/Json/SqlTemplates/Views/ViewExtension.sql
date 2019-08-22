c1c8f353524552387992a23b39f615cb70dee095
paulstancer <pstancer@hotmail.com>
2019-08-22 18:19:19 +0100

    Minor update for SQL View template data

--- Src/Xigadee.Azure.Sql/Persistence/Json/SqlTemplates/Views/ViewExtension.sql
+++ Src/Xigadee.Azure.Sql/Persistence/Json/SqlTemplates/Views/ViewExtension.sql
@@ -1,7 +1,14 @@
 ﻿--#region.extension
 CREATE VIEW [{NamespaceTable}].[View{EntityName}]
 AS
-	SELECT E.*
+	SELECT E.[Id] 
+	,E.[ExternalId] 
+    ,E.[VersionId] 
+    ,E.[UserIdAudit] 
+	,E.[DateCreated] 
+	,E.[DateUpdated] 
+	,E.[Sig] 
+	,E.[Body] 
 	FROM [{NamespaceTable}].[{EntityName}] AS E 
 	INNER JOIN [{NamespaceTable}].[{EntityName}_Extension] AS EX ON E.Id = EX.EntityId
 GO
