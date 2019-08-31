1dbd2da83e2c217d622e1533855bcb3b3cf4522d
paulstancer <pstancer@hotmail.com>
2019-08-31 12:31:47 +0100

    Tidy up of helpers

--- Src/Xigadee/Communication/FabricBridge/TcpTlsChannel/Messaging/InternetMessage/Mime/Fragments/MimeMessageFragment.cs
+++ Src/Xigadee/Communication/FabricBridge/TcpTlsChannel/Messaging/InternetMessage/Mime/Fragments/MimeMessageFragment.cs
@@ -1,33 +1,4 @@
-﻿#region Copyright
-// Copyright Hitachi Consulting
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//    http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-#endregion
-
-using System;
-using System.Collections;
-using System.Net;
-using System.Net.Sockets;
-using System.Net.Security;
-using System.Security.Authentication;
-using System.Text;
-using System.Security.Cryptography.X509Certificates;
-using System.IO;
-using System.Collections.Generic;
-using System.Collections.Concurrent;
-using System.Net.Http;
-using System.Threading.Tasks;
-using System.Linq;
+﻿using System.IO;
 
 namespace Xigadee
 {
