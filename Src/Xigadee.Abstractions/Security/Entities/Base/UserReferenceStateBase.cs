4f5fffa2a1f23dab89f2a1ea16769360d273f6dc
paulstancer <pstancer@hotmail.com>
2019-10-30 13:42:30 +0000

    Consolidating State change in to a common pattern.

---
+++ Src/Xigadee.Abstractions/Security/Entities/Base/UserReferenceStateBase.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Xigadee
+{
+    /// <summary>
+    /// This class extends the user reference class with state history support.
+    /// </summary>
+    public abstract class UserReferenceStateBase : UserReferenceBase
+    {
+        #region StateChanges ...
+        /// <summary>
+        /// This is the list of historical state changes.
+        /// </summary>
+        public virtual List<StateChangeHistory> StateChanges { get; set; } = new List<StateChangeHistory>();
+        /// <summary>
+        /// This method changes the state to the new state and sets the state change history.
+        /// </summary>
+        /// <param name="newState">The new state.</param>
+        /// <param name="description">The optional description.</param>
+        /// <param name="memberName">The caller member name.</param>
+        /// <param name="sourceLineNumber">The caller source line number.</param>
+        public virtual void StateChange(string newState, string description = null
+            , [CallerMemberName] string memberName = "", [CallerLineNumber] int sourceLineNumber = 0)
+        {
+            var sc = new StateChangeHistory();
+
+            sc.StateOld = State;
+            sc.StateNew = newState;
+            sc.Description = description;
+
+            sc.Caller = $"{memberName}@{sourceLineNumber}";
+
+            State = newState;
+
+            StateChanges.Add(sc);
+        }
+        #endregion
+
+        #region State
+        /// <summary>
+        /// Gets or sets a value indicating the stage of the transfer.
+        /// </summary>
+        public virtual string State { get; set; }
+        #endregion
+    }
+
+    /// <summary>
+    /// This is the shared state change base.
+    /// </summary>
+    public class StateChangeHistory
+    {
+        /// <summary>
+        /// The original state.
+        /// </summary>
+        public string StateOld { get; set; }
+        /// <summary>
+        /// The new state.
+        /// </summary>
+        public string StateNew { get; set; }
+        /// <summary>
+        /// The UTC time for the change.
+        /// </summary>
+        public DateTime TimeStamp { get; set; } = DateTime.UtcNow;
+        /// <summary>
+        /// The optional description.
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// This is the caller information.
+        /// </summary>
+        public string Caller { get; set; }
+    }
+}
--- Src/Xigadee.Abstractions/Security/Entities/UserExternalAction.cs
+++ Src/Xigadee.Abstractions/Security/Entities/UserExternalAction.cs
@@ -8,7 +8,7 @@ namespace Xigadee
     /// This class is used to store an async action for a user account, such as account creation confirmation, and/or 
     /// password reset completion.
     /// </summary>
-    public class UserExternalAction: UserReferenceBase
+    public class UserExternalAction: UserReferenceStateBase
     {
         /// <summary>
         /// Gets or sets the type of the action, i.e. Email, SMS, Push etc.
@@ -26,59 +26,12 @@ namespace Xigadee
         /// This property holds the current state of the action.
         /// </summary>
         [EntityPropertyHint("state")]
-        public string State { get; set; }
+        public override string State { get => base.State; set => base.State = value; }
 
         /// <summary>
         /// Gets or sets the option expiry date/time for the action.
         /// </summary>
         public DateTime? DateExpiry { get; set; }
 
-        #region StateChanges ...
-        /// <summary>
-        /// This is the list of historical state changes.
-        /// </summary>
-        public List<UserExternalActionStateChange> StateChanges { get; set; } = new List<UserExternalActionStateChange>();
-        /// <summary>
-        /// This method changes the state to the new state and sets the state change history.
-        /// </summary>
-        /// <param name="newState">The new state.</param>
-        /// <param name="description">The optional description.</param>
-        public void StateChange(string newState, string description = null)
-        {
-            var sc = new UserExternalActionStateChange();
-
-            sc.StateOld = State;
-            sc.StateNew = newState;
-            sc.Description = description;
-
-            State = newState;
-
-            StateChanges.Add(sc);
-        }
-        #endregion
-    }
-
-    /// <summary>
-    /// This helper class holds the state change information.
-    /// </summary>
-    [DebuggerDisplay("{StateOld}->{StateNew} @ {TimeStamp}: {Description}")]
-    public class UserExternalActionStateChange
-    {
-        /// <summary>
-        /// The original state.
-        /// </summary>
-        public string StateOld { get; set; }
-        /// <summary>
-        /// The new state.
-        /// </summary>
-        public string StateNew { get; set; }
-        /// <summary>
-        /// The UTC time for the change.
-        /// </summary>
-        public DateTime TimeStamp { get; set; } = DateTime.UtcNow;
-        /// <summary>
-        /// The optional description.
-        /// </summary>
-        public string Description { get; set; }
     }
 }
