22b1f744e8116c0dc28634f40e2141f130941526
paulstancer <pstancer@hotmail.com>
2019-11-28 16:35:00 +0000

    Minor changes for the event.

--- Src/Xigadee.AspNetCore/Logging/Events/LogEventApplication.cs
+++ Src/Xigadee.AspNetCore/Logging/Events/LogEventApplication.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Microsoft.Extensions.Logging;
 
 namespace Xigadee
@@ -7,24 +8,44 @@ namespace Xigadee
     /// <summary>
     /// Standard log event for microservices.
     /// </summary>
+    [DebuggerDisplay("{LogLevel} {Name}: {Message}")]
     public class LogEventApplication
     {
+        /// <summary>
+        /// This is the logging level of the event.
+        /// </summary>
         public LogLevel LogLevel { get; set; }
-
+        /// <summary>
+        /// This is the event id
+        /// </summary>
         public EventId EventId { get; set; }
-
+        /// <summary>
+        /// This is the optional state object that can be set to the event 
+        /// </summary>
         public object State { get; set; }
-
+        /// <summary>
+        /// This is the optional exception that was raised for the event.
+        /// </summary>
         public Exception Exception { get; set; }
-
+        /// <summary>
+        /// The is the event message.
+        /// </summary>
         public string Message { get; set; }
-
+        /// <summary>
+        /// This is the event name.
+        /// </summary>
         public string Name { get; set; }
-
+        /// <summary>
+        /// This is the operation id of the event.
+        /// </summary>
         public string OperationId { get; set; }
-
+        /// <summary>
+        /// This is the parent id of the event.
+        /// </summary>
         public string ParentId { get; set; }
-
+        /// <summary>
+        /// This dictionary contains any additional formatted parameters for the event.
+        /// </summary>
         public Dictionary<string, object> FormattedParameters { get; set; } = new Dictionary<string, object>();
     }
 }
