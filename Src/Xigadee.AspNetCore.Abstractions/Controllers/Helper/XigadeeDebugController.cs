fc3c612aad6a44f3bdffa22a216cde422840b0de
Paul Stancer <paul@TiPJAR.onmicrosoft.com>
2021-03-30 11:53:34 +0100

    Added base controller.

---
+++ Src/Xigadee.AspNetCore.Abstractions/Controllers/Helper/XigadeeDebugController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+using Xigadee;
+namespace Xigadee
+{ 
+    /// <summary>
+    /// This controller handles incoming Stripe callback requests.
+    /// </summary>
+    public abstract class XigadeeDebugControllerBase : ApiControllerBase
+    {
+        StatisticsHolder _holder;
+        ConfigHealthCheck _config;
+
+        #region Constructor
+        /// <summary>
+        /// This is the default controller.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        /// <param name="holder">The statistics holder.</param>
+        /// <param name="config">The health check configuration.</param>
+        protected XigadeeDebugControllerBase(ILogger logger, StatisticsHolder holder, ConfigHealthCheck config) : base(logger)
+        {
+            _holder = holder;
+            _config = config;
+        }
+        #endregion
+
+        /// <summary>
+        /// This method takes the Stripe postbacks and stores them for later processing.
+        /// </summary>
+        [HttpGet("{id}")]
+        public virtual Task<IActionResult> Output(Guid id)
+        {
+            if (!_config.Enabled
+                || id != _config.Id
+                || _holder?.Statistics == null)
+            {
+                _logger.LogError($"XigadeeDebug failure {id}");
+                return Task.FromResult<IActionResult>(StatusCode(404));
+            }
+
+            _logger.LogInformation("XigadeeDebug success");
+            return Task.FromResult<IActionResult>(StatusCode(200, _holder.Statistics));
+        }
+    }
+}
